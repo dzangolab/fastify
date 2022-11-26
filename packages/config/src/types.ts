2a061c3e23dec2fbdeba604e5b19b0b7271f13df
Olivier Pichon <op@dzango.com>
2022-11-26 17:53:26 +0700

    refactor(config): restore db, graphql and supertokens ApiConfig properties

--- packages/config/src/types.ts
+++ packages/config/src/types.ts
@@ -2,7 +2,23 @@ export interface ApiConfig {
   appName: string;
   appOrigin: string[];
   baseUrl: string;
+  db: {
+    acquireConnectionTimeout: number;
+    databaseName: string;
+    debug: boolean;
+    host: string;
+    password: string;
+    port: number;
+    username: string;
+  };
   env: string;
+  graphql: {
+    enabled: boolean;
+    path: string;
+  };
+  graphiql: {
+    enabled: boolean;
+  };
   logger: {
     level: string;
     transport?: {
@@ -19,6 +35,15 @@ export interface ApiConfig {
   protocol: string;
   rest: {
     enabled: boolean;
+  };
+  supertokens: {
+    connectionUri: string;
+    providers: {
+      google: {
+        client_id: string;
+        client_secret: string;
+      };
+    };
   };
   version: string;
 }
