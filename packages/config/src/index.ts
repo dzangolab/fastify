abb20392ce18a6f8471a2dd00313c673b5fb00d3
Olivier Pichon <op@dzango.com>
2022-12-04 11:02:19 +0700

    fix(config): extract plugin as separate file

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,27 +1,4 @@
-import FastifyPlugin from "fastify-plugin";
-
 import type { ApiConfig } from "./types";
-import type { FastifyInstance, FastifyRequest } from "fastify";
-
-const plugin = async (
-  fastify: FastifyInstance,
-  options: { config: ApiConfig },
-  done: () => void
-) => {
-  const config = options.config;
-
-  // Decorate api and request with `config`
-  fastify.decorate("config", config);
-  fastify.addHook("onRequest", async (request: FastifyRequest) => {
-    request.config = config;
-  });
-
-  const { baseUrl, port } = config;
-  const host = `${baseUrl}:${port}`;
-  fastify.decorate("hostname", host);
-
-  done();
-};
 
 declare module "fastify" {
   interface FastifyInstance {
@@ -34,8 +11,8 @@ declare module "fastify" {
   }
 }
 
-export default FastifyPlugin(plugin);
-
 export { default as parse } from "./parse";
 
 export type { ApiConfig } from "./types";
+
+export { default } from "./plugin";
