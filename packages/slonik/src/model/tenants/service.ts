ec48c1ea17d44d80384d860d1947678d293ea45b
Dipendra Upreti <dipendra@dzangolab.com>
2023-01-06 18:03:51 +0545

    feat(slonik): create schema and run migration when new tenant created

--- packages/slonik/src/model/tenants/service.ts
+++ packages/slonik/src/model/tenants/service.ts
@@ -1,6 +1,8 @@
 import { ApiConfig } from "@dzangolab/fastify-config";
 
+import runMigrations from "../../runMigrations";
 import SqlFactory from "../../sqlFactory";
+import getMigrateDatabaseConfig from "../../utils/getMigrateDatabaseConfig";
 
 import type { Database, Tenant, TenantInput } from "../../types";
 import type { SqlTaggedTemplate } from "slonik";
@@ -28,11 +30,20 @@ const TenantService = (
     create: async (data: TenantInput): Promise<Tenant> => {
       const query = factory.create(data);
 
-      return await database.connect(async (connection) => {
+      const result = await database.connect(async (connection) => {
         return connection.query(query).then((data) => {
           return data.rows[0];
         });
       });
+
+      // Run migrations on created tenant.
+      await runMigrations(
+        getMigrateDatabaseConfig(config.slonik),
+        config.slonik.migrations + "/tenants",
+        result.slug
+      );
+
+      return result;
     },
 
     delete: async (id: number): Promise<Tenant | null> => {
@@ -40,6 +51,13 @@ const TenantService = (
 
       const result = await database.connect((connection) => {
         return connection.one(query);
+      });
+
+      // Delete the schema
+      const deleteQuery = sql`DROP SCHEMA IF EXISTS ${result.slug} CASCADE`;
+
+      await database.connect((connection) => {
+        return connection.query(deleteQuery);
       });
 
       return result;
