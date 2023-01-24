c04f0dce6fa50899f9025bdbc2a42dee76654e45
Dipendra Upreti <dipendra@dzangolab.com>
2023-01-24 15:16:28 +0545

    feat(mulit-tenant): refactor changeSchema.ts file

--- packages/multi-tenant/src/lib/changeSchema.ts
+++ packages/multi-tenant/src/lib/changeSchema.ts
@@ -1,11 +1,13 @@
 import type { Client } from "pg";
 
 const changeSchema = async (client: Client, name: string) => {
-  // Create schema if not exists
-  await client.query(`CREATE SCHEMA IF NOT EXISTS ${name};`);
-
-  // Switch to the schema
-  await client.query(`SET search_path TO ${name};`);
+  // Create schema if not exists and Switch to the schema
+  await client.query(
+    `
+      CREATE SCHEMA IF NOT EXISTS ${name};
+      SET search_path TO ${name};
+    `
+  );
 };
 
 export default changeSchema;
