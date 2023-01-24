3d269012b3ac0a571933479e70077fcee16c7e60
Dipendra Upreti <dipendra@dzangolab.com>
2023-01-24 15:33:14 +0545

    refactor(multi-tenant): refactor service.ts file

--- packages/multi-tenant/src/lib/changeSchema.ts
+++ packages/multi-tenant/src/lib/changeSchema.ts
@@ -1,7 +1,7 @@
 import type { Client } from "pg";
 
 const changeSchema = async (client: Client, name: string) => {
-  // Create schema if not exists and Switch to the schema
+  // Create schema if not exists and switch to the schema
   await client.query(
     `
       CREATE SCHEMA IF NOT EXISTS ${name};
--- packages/multi-tenant/src/model/tenants/service.ts
+++ packages/multi-tenant/src/model/tenants/service.ts
@@ -26,68 +26,66 @@ const TenantService = (
 
   const factory = SqlFactory<Tenant, TenantInput>(sql, tableName, config);
 
-  return {
-    all: async (): Promise<readonly Tenant[]> => {
-      const query = factory.all(columns);
+  const all = async (): Promise<readonly Tenant[]> => {
+    const query = factory.all(columns);
 
-      const result = await database.connect(
-        (connection: DatabasePoolConnection) => {
-          return connection.any(query);
-        }
-      );
-
-      return result;
-    },
-    create: async (tenantInput: TenantInput): Promise<Tenant> => {
-      if (!tenantInput[slugColumn]) {
-        throw new Error(`${slugColumn} missing`);
+    const result = await database.connect(
+      (connection: DatabasePoolConnection) => {
+        return connection.any(query);
       }
+    );
 
-      if (
-        await TenantService(config, database, sql).findOneBySlug(
-          tenantInput[slugColumn]
-        )
-      ) {
-        throw new Error(
-          `${tenantInput[slugColumn]} ${slugColumn} already exists`
-        );
-      }
-
-      const query = factory.create(tenantInput);
-
-      const result = await database.connect(
-        async (connection: DatabasePoolConnection) => {
-          return connection.query(query).then((data) => {
-            return data.rows[0];
-          });
-        }
-      );
-
-      // run migration on created tenant
-      await runMigrations(
-        getDatabaseConfig(config.slonik),
-        multiTenantConfig.migrations.path,
-        tenantInput[slugColumn]
-      );
-
-      return result;
-    },
-    findOneBySlug: async (slug: string): Promise<Tenant | null> => {
-      const query = sql<Tenant>`
-        SELECT *
-        FROM ${createTableFragment(tableName)}
-        WHERE ${sql.identifier([slugColumn])} = ${slug};
-      `;
-
-      const result = await database.connect(
-        (connection: DatabasePoolConnection) => {
-          return connection.maybeOne(query);
-        }
-      );
-
-      return result;
-    },
+    return result;
   };
+
+  const create = async (tenantInput: TenantInput): Promise<Tenant> => {
+    if (!tenantInput[slugColumn]) {
+      throw new Error(`${slugColumn} missing`);
+    }
+
+    if (await findOneBySlug(tenantInput[slugColumn])) {
+      throw new Error(
+        `${tenantInput[slugColumn]} ${slugColumn} already exists`
+      );
+    }
+
+    const query = factory.create(tenantInput);
+
+    const result = await database.connect(
+      async (connection: DatabasePoolConnection) => {
+        return connection.query(query).then((data) => {
+          return data.rows[0];
+        });
+      }
+    );
+
+    // run migration on created tenant
+    await runMigrations(
+      getDatabaseConfig(config.slonik),
+      multiTenantConfig.migrations.path,
+      tenantInput[slugColumn]
+    );
+
+    return result;
+  };
+
+  const findOneBySlug = async (slug: string): Promise<Tenant | null> => {
+    const query = sql<Tenant>`
+      SELECT *
+      FROM ${createTableFragment(tableName)}
+      WHERE ${sql.identifier([slugColumn])} = ${slug};
+    `;
+
+    const result = await database.connect(
+      (connection: DatabasePoolConnection) => {
+        return connection.maybeOne(query);
+      }
+    );
+
+    return result;
+  };
+
+  return { all, create, findOneBySlug };
 };
 
 export default TenantService;
