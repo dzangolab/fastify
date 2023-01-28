c7c9d27b089eb9805eef802e53f6750dbaf7bc66
Olivier Pichon <op@dzango.com>
2023-01-28 20:30:45 +0700

    feat(slonik): add fieldNameCaseConverter interceptor

---
+++ packages/slonik/src/factories/__test__/createClientConfiguration.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, it } from "vitest";
+
+import fieldNameCaseConverter from "../../interceptors/fieldNameCaseConverter";
+import createClientConfiguration from "../createClientConfiguration";
+
+import type { Query, QueryContext } from "slonik";
+
+describe("createClientConfiguration helper", () => {
+  const defaultConfiguration = {
+    captureStackTrace: false,
+    connectionRetryLimit: 3,
+    connectionTimeout: 5000,
+    idleInTransactionSessionTimeout: 60000,
+    idleTimeout: 5000,
+    interceptors: [fieldNameCaseConverter],
+    maximumPoolSize: 10,
+    queryRetryLimit: 5,
+    statementTimeout: 60000,
+    transactionRetryLimit: 5,
+  };
+
+  it("creates default configuration", () => {
+    const configuration = createClientConfiguration();
+
+    expect(configuration).toEqual(defaultConfiguration);
+  });
+
+  it("includes fieldNameCaseConvertor interceptor", () => {
+    const interceptor = {
+      transformQuery: (context: QueryContext, query: Query): Query => {
+        return query;
+      },
+    };
+
+    const configuration = createClientConfiguration({
+      interceptors: [interceptor],
+    });
+
+    expect(configuration.interceptors).toContain(fieldNameCaseConverter);
+  });
+});
