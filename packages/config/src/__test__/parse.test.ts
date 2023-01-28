c924ad9b1644a4742c3912d395756b1f3dc25a37
Olivier Pichon <203745+opichon@users.noreply.github.com>
2023-01-28 23:20:56 +0700

    Config/tests (#185)
    * feat(slonik): add fieldNameCaseConverter interceptor
    
    * test(config): add unit tests for parser
    
    * feat(slonik)!: camelize query result
    
    BREAKING CHANGE: SqlFactory arguments have changed.
    
    * fix(multi-tenant): update service factory
    
    * chore(slonik): cleanup configuration
    
    * chore(config): cleanup tsconfig

---
+++ packages/config/src/__test__/parse.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from "vitest";
+
+import parse from "../parse";
+
+describe("parse", () => {
+  it("parses a boolean", () => {
+    expect(parse("false", false)).toBe(false);
+  });
+
+  it("parses a number", () => {
+    expect(parse("23", 1)).toBe(23);
+  });
+
+  it("parses a number as a string", () => {
+    expect(parse("23", "abc")).toBe("23");
+  });
+
+  it("parses 'false' as a string", () => {
+    expect(parse("false", "abc")).toBe("false");
+  });
+
+  it("returns the fallback value as a boolean", () => {
+    expect(parse(undefined, true)).toBe(true);
+  });
+
+  it("returns the fallback value as a number", () => {
+    expect(parse(undefined, 123)).toBe(123);
+  });
+
+  it("returns the fallback value as a string", () => {
+    expect(parse(undefined, "abc")).toBe("abc");
+  });
+});
