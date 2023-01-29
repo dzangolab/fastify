14f0faec12970630ced71dffa9cd660963df49ea
Olivier Pichon <203745+opichon@users.noreply.github.com>
2023-01-29 18:47:58 +0700

    Slonik/sqlfactory/refactor (#189)
    * refactor(slonik): refactor SqlFactory as a class
    
    * chore: ignore test files and helpers in coverage report
    
    * chore(slonik): upgrade slonik to 30.4.0 (#190)
    
    * chore(slonik): upgrade slonik to 30.4.0
    
    * chore(mercurius): update slonik to 30.4.0

---
+++ packages/multi-tenant/src/model/tenants/__test__/helpers/createConfig.ts
@@ -0,0 +1,27 @@
+/* istanbul ignore file */
+import type {
+  MultiTenantConfig,
+  MultiTenantEnabledConfig,
+} from "../../../../types";
+
+const createConfig = (multiTenantConfig?: MultiTenantConfig) => {
+  const config: MultiTenantEnabledConfig = {
+    pagination: {
+      default_limit: 10,
+      max_limit: 50,
+    },
+    slonik: {
+      db: {
+        databaseName: "test",
+        host: "localhost",
+        password: "password",
+        username: "username",
+      },
+    },
+    multiTenant: multiTenantConfig,
+  };
+
+  return config;
+};
+
+export default createConfig;
--- packages/slonik/src/interceptors/__test__/fieldNameCaseConverter.test.ts
+++ packages/slonik/src/interceptors/__test__/fieldNameCaseConverter.test.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 import { describe, expect, it } from "vitest";
 
 import createQueryContext from "./helpers/createQueryContext";
