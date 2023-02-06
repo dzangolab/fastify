85d6840f17dbaa8e0f6610b7c1186735e37d22e1
Dipendra Upreti <dipendra@dzangolab.com>
2023-02-06 18:13:43 +0545

    refactor(multi-tenant): refactor getHost test

--- packages/multi-tenant/src/lib/__test__/getHost.test.ts
+++ packages/multi-tenant/src/lib/__test__/getHost.test.ts
@@ -2,44 +2,18 @@ import { describe, expect, it } from "vitest";
 
 import getHost from "../getHost";
 
-describe("getHost", () => {
-  // it("should return same", () => {
-  //   expect(getHost("example.test")).toEqual("example.test");
-  // });
-
-  it("should remove https", () => {
-    expect(getHost("https://example.test")).toEqual("example.test");
-  });
-
-  it("should remove path", () => {
-    expect(getHost("https://example.test/users/1")).toEqual("example.test");
-  });
-
-  it("should include subdomain", () => {
-    expect(getHost("https://api.example.test")).toEqual("api.example.test");
-  });
-
-  it("should include subdomain and remove path", () => {
-    expect(getHost("https://api.example.test/user/1")).toEqual(
-      "api.example.test"
-    );
-  });
-
-  it("should return empty", () => {
-    expect(getHost("")).toEqual("");
-  });
-
-  it("should return when invalid url passed", () => {
-    expect(getHost("example")).toEqual("example");
-  });
-
-  it("should include port", () => {
-    expect(getHost("http://example.test:8080")).toEqual("example.test:8080");
-  });
-
-  it("should include port and ignore path", () => {
-    expect(getHost("http://example.test:8080/path")).toEqual(
-      "example.test:8080"
-    );
+describe.concurrent("getHost", () => {
+  it.each([
+    ["example.test", "example.test"],
+    ["https://example.test", "example.test"],
+    ["https://example.test/users/1", "example.test"],
+    ["https://api.example.test", "api.example.test"],
+    ["https://api.example.test/user/1", "api.example.test"],
+    ["", ""],
+    ["example", "example"],
+    ["http://example.test:8080", "example.test:8080"],
+    ["http://example.test:8080/path", "example.test:8080"],
+  ])("getHost(url) -> domain", async (url, expected) => {
+    expect(getHost(url)).toBe(expected);
   });
 });
