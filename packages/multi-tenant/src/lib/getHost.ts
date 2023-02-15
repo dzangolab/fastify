594889d220a112952745402b8f8f2745899ccacc
ramesh <ramesh@dzangolab.com>
2023-02-15 16:29:07 +0545

    fix: getHost module

--- packages/multi-tenant/src/lib/getHost.ts
+++ packages/multi-tenant/src/lib/getHost.ts
@@ -3,6 +3,11 @@ const getHost = (url: string) => {
 
   try {
     host = new URL(url).host;
+
+    // RL[2023-02-15] host will be empty for url localhost with port e.g. `localhost:3000`.
+    if (!host) {
+      throw new Error("Host is empty");
+    }
   } catch {
     host = url;
   }
