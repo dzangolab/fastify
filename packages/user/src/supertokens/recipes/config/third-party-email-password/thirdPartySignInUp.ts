2985954421a8f7cbd671eac18369fdbe2963cc80
Dipendra Upreti <dipendra@dzangolab.com>
2023-02-27 15:03:43 +0545

    feat(usr): add support for multi-tenant for thirdparty auth

--- packages/user/src/supertokens/recipes/config/third-party-email-password/thirdPartySignInUp.ts
+++ packages/user/src/supertokens/recipes/config/third-party-email-password/thirdPartySignInUp.ts
@@ -1,5 +1,7 @@
 import { getUserByThirdPartyInfo } from "supertokens-node/recipe/thirdpartyemailpassword";
 
+import updateEmail from "../../../utils/updateEmail";
+
 import type { FastifyInstance, FastifyError } from "fastify";
 import type { RecipeInterface } from "supertokens-node/recipe/thirdpartyemailpassword";
 
@@ -7,16 +9,16 @@ const thirdPartySignInUp = (
   originalImplementation: RecipeInterface,
   fastify: FastifyInstance
 ): typeof originalImplementation.thirdPartySignInUp => {
-  const { config } = fastify;
-
   return async (input) => {
+    input.email = updateEmail.appendTenantId(input.email, fastify.tenant);
+
     const user = await getUserByThirdPartyInfo(
       input.thirdPartyId,
       input.thirdPartyUserId,
       input.userContext
     );
 
-    if (!user && config.user.features?.signUp === false) {
+    if (!user && fastify.config.user.features?.signUp === false) {
       throw {
         name: "SIGN_UP_DISABLED",
         message: "SignUp feature is currently disabled",
