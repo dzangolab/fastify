073e29aedd0a7b9ba47c4d44ae5d0eb19874d030
Dipendra Upreti <dipendra@dzangolab.com>
2023-02-28 17:04:54 +0545

    chore(user): fix code

--- packages/user/src/supertokens/utils/updateThirdPartyUserId.ts
+++ packages/user/src/supertokens/utils/updateThirdPartyUserId.ts
@@ -1,4 +1,4 @@
-import { Tenant } from "@dzangolab/fastify-multi-tenant";
+import type { Tenant } from "@dzangolab/fastify-multi-tenant";
 
 const updateThirdPartyUserId = {
   appendTenantId: (userid: string, tenant: Tenant) => {
