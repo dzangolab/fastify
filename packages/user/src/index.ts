aa3331a9c8c8d2a32b30a87b961235baaed3fdf8
Nabin Dhital <dhitalnabin11@gmail.com>
2023-02-28 10:12:31 +0545

    feat(user): add emailPassword property in user features

--- packages/user/src/index.ts
+++ packages/user/src/index.ts
@@ -14,6 +14,7 @@ declare module "@dzangolab/fastify-config" {
       supertokens: SupertokensConfig;
       features?: {
         signUp?: boolean;
+        emailPassword?: boolean;
       };
     };
   }
