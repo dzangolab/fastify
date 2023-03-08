c9347e8526f7bd7f10a2499a861947bce906af0e
Dipendra Upreti <dipendra@dzangolab.com>
2023-03-08 14:53:11 +0545

    feat(user): remove updateTenantUser

--- packages/user/src/supertokens/recipes/config/third-party-email-password/getUserById.ts
+++ packages/user/src/supertokens/recipes/config/third-party-email-password/getUserById.ts
@@ -1,4 +1,4 @@
-import updateTenantUser from "../../../utils/updateTenantUser";
+import updateEmail from "../../.././utils/updateEmail";
 
 import type { RecipeInterface } from "supertokens-node/recipe/thirdpartyemailpassword";
 
@@ -9,7 +9,10 @@ const getUserById = (
     let user = await originalImplementation.getUserById(input);
 
     if (user && input.userContext.tenant) {
-      user = updateTenantUser(user, input.userContext.tenant);
+      user = {
+        ...user,
+        email: updateEmail.removeTenantId(user.email, input.userContext.tenant),
+      };
     }
 
     return user;
