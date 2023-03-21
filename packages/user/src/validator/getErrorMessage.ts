50f678629dac2d59a4c9c2b15628519239efe83d
Dipendra Upreti <dipendra@dzangolab.com>
2023-03-21 21:24:19 +0545

    refactor(user): improve getErrorMessage

--- packages/user/src/validator/getErrorMessage.ts
+++ packages/user/src/validator/getErrorMessage.ts
@@ -8,26 +8,69 @@ const getErrorMessage = (options?: StrongPasswordOptions): string => {
   const messages: string[] = [];
 
   if (options.minLength) {
-    messages.push(`minimum ${options.minLength} character`);
+    const length = options.minLength;
+
+    if (length > 1) {
+      messages.push(`minimum ${length} characters`);
+    } else {
+      messages.push(`minimum ${length} character`);
+    }
   }
 
   if (options.minLowercase) {
-    messages.push(`minimum ${options.minLowercase} lowercase`);
+    const length = options.minLowercase;
+
+    if (length > 1) {
+      messages.push(`minimum ${length} lowercases`);
+    } else {
+      messages.push(`minimum ${length} lowercase`);
+    }
   }
 
   if (options.minUppercase) {
-    messages.push(`minimum ${options.minUppercase} uppercase`);
+    const length = options.minUppercase;
+
+    if (length > 1) {
+      messages.push(`minimum ${length} uppercases`);
+    } else {
+      messages.push(`minimum ${length} uppercase`);
+    }
   }
 
   if (options.minNumbers) {
-    messages.push(`minimum ${options.minNumbers} number`);
+    const length = options.minNumbers;
+
+    if (length > 1) {
+      messages.push(`minimum ${length} numbers`);
+    } else {
+      messages.push(`minimum ${length} number`);
+    }
   }
 
   if (options.minSymbols) {
-    messages.push(`minimum ${options.minSymbols} symbol`);
+    const length = options.minSymbols;
+
+    if (length > 1) {
+      messages.push(`minimum ${length} symbols`);
+    } else {
+      messages.push(`minimum ${length} symbol`);
+    }
   }
 
-  return "Passsword should contain " + messages.join(", ");
+  let message: string;
+
+  if (messages.length === 0) {
+    message = "Password is too weak";
+  } else if (messages.length === 1) {
+    message = "Passsword should contain " + messages[0];
+  } else {
+    const lastMessage = messages.pop();
+
+    message =
+      "Passsword should contain " + messages.join(", ") + " and " + lastMessage;
+  }
+
+  return message;
 };
 
 export default getErrorMessage;
