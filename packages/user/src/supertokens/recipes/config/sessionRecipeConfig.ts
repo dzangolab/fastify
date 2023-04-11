59595e813bdebc3f212a0868aa7194d31fbc6c54
ramesh <ramesh@dzangolab.com>
2023-04-11 17:41:22 +0545

    refactor: refactor session recipe config

--- packages/user/src/supertokens/recipes/config/sessionRecipeConfig.ts
+++ packages/user/src/supertokens/recipes/config/sessionRecipeConfig.ts
@@ -1,22 +1,7 @@
 import type { TypeInput as SessionRecipeConfig } from "supertokens-node/recipe/session/types";
 
 const getSessionRecipeConfig = (): SessionRecipeConfig => {
-  return {
-    override: {
-      functions: function (originalImplementation) {
-        return {
-          ...originalImplementation,
-          createNewSession: async function (input) {
-            input.accessTokenPayload = {
-              ...input.accessTokenPayload,
-            };
-
-            return originalImplementation.createNewSession(input);
-          },
-        };
-      },
-    },
-  };
+  return {};
 };
 
 export default getSessionRecipeConfig;
