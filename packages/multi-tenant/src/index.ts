35855d91292d805cc862e71ae42710ff3d02ea00
Dipendra Upreti <dipendra@dzangolab.com>
2023-04-12 09:32:50 +0545

    feat(multi-tenant): export thirdPartyEmailPassword config

--- packages/multi-tenant/src/index.ts
+++ packages/multi-tenant/src/index.ts
@@ -25,6 +25,8 @@ export { default } from "./plugin";
 
 export { default as TenantService } from "./model/tenants/service";
 
+export { default as thirdPartyEmailPassword } from "./config";
+
 export type {
   MultiTenantConfig,
   Tenant,
