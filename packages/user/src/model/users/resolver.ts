9e78a739b8fb460f398a6752504f2dacc5499b7f
Dipendra Upreti <dipendra@dzangolab.com>
2023-04-13 17:12:13 +0545

    feat(user): add update user profile graphql query

--- packages/user/src/model/users/resolver.ts
+++ packages/user/src/model/users/resolver.ts
@@ -2,6 +2,7 @@ import mercurius from "mercurius";
 
 import Service from "./service";
 
+import type { UserProfileUpdateInput } from "./../../types";
 import type { MercuriusContext } from "mercurius";
 
 const Mutation = {
@@ -16,14 +17,45 @@ const Mutation = {
     const service = new Service(context.config, context.database);
 
     try {
+      return context.user?.id
+        ? await service.changePassword(
+            context.user.id,
+            arguments_.oldPassword,
+            arguments_.newPassword
+          )
+        : {
+            status: "NOT_FOUND",
+            message: "User not found",
+          };
+    } catch (error) {
+      // FIXME [OP 28 SEP 2022]
+      context.app.log.error(error);
+
+      const mercuriusError = new mercurius.ErrorWithProps(
+        "Oops, Something went wrong"
+      );
+      mercuriusError.statusCode = 500;
+
+      return mercuriusError;
+    }
+  },
+  updateUserProfile: async (
+    parent: unknown,
+    arguments_: {
+      data: UserProfileUpdateInput;
+    },
+    context: MercuriusContext
+  ) => {
+    const service = new Service(context.config, context.database);
+
+    try {
       if (context.user?.id) {
-        const changePasswordResponse = await service.changePassword(
-          context.user?.id,
-          arguments_.oldPassword,
-          arguments_.newPassword
+        const updateProfileResponse = await service.update(
+          context.user.id,
+          arguments_.data
         );
 
-        return changePasswordResponse;
+        return updateProfileResponse;
       } else {
         return {
           status: "NOT_FOUND",
