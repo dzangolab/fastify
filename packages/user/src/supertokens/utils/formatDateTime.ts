96f11db1932c21da450110fd1af1d79ff01ed99f
Dipendra Upreti <dipendra@dzangolab.com>
2023-04-25 12:18:07 +0545

    test(user): add formatDateTime test

--- packages/user/src/supertokens/utils/formatDateTime.ts
+++ packages/user/src/supertokens/utils/formatDateTime.ts
@@ -1,5 +1,5 @@
 const formatDateTime = (date: Date) => {
-  return date.toISOString().slice(0, 19).replace("T", " ") as unknown as number;
+  return date.toISOString().slice(0, 23).replace("T", " ") as unknown as number;
 };
 
 export default formatDateTime;
