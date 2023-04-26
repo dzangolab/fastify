9042cb6a3ec66bfa6c53c27c11ccb662c5a81b5e
Dipendra Upreti <dipendra@dzangolab.com>
2023-04-26 10:40:39 +0545

    refactor(user): rename function formatDate

--- packages/user/src/index.ts
+++ packages/user/src/index.ts
@@ -29,8 +29,9 @@ declare module "@dzangolab/fastify-config" {
 
 export { default } from "./plugin";
 
-export type { AuthUser, UserCreateInput, UserUpdateInput, User } from "./types";
-
 export { default as userResolver } from "./model/users/resolver";
 export { default as UserService } from "./model/users/service";
 export { default as userRoutes } from "./model/users/controller";
+export { default as formatDate } from "./supertokens/utils/formatDate";
+
+export type { AuthUser, UserCreateInput, UserUpdateInput, User } from "./types";
