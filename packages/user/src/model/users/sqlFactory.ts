d5dfbe6197ac6ad1f76f0e19eb951b83ff1ad3eb
Dipendra Upreti <dipendra@dzangolab.com>
2023-05-23 09:52:15 +0545

    feat(user): add roles in users endpoint

--- packages/user/src/model/users/sqlFactory.ts
+++ packages/user/src/model/users/sqlFactory.ts
@@ -1,7 +1,17 @@
-import { DefaultSqlFactory } from "@dzangolab/fastify-slonik";
+import {
+  DefaultSqlFactory,
+  createLimitFragment,
+  createFilterFragment,
+  createSortFragment,
+  createTableIdentifier,
+} from "@dzangolab/fastify-slonik";
+import { QueryResultRow, QuerySqlToken, sql } from "slonik";
 
-import type { SqlFactory } from "@dzangolab/fastify-slonik";
-import type { QueryResultRow } from "slonik";
+import type {
+  SqlFactory,
+  FilterInput,
+  SortInput,
+} from "@dzangolab/fastify-slonik";
 
 /* eslint-disable brace-style */
 class UserSqlFactory<
@@ -10,8 +20,34 @@ class UserSqlFactory<
     UserUpdateInput extends QueryResultRow
   >
   extends DefaultSqlFactory<User, UserCreateInput, UserUpdateInput>
-  implements SqlFactory<User, UserCreateInput, UserUpdateInput> {
+  implements SqlFactory<User, UserCreateInput, UserUpdateInput>
+{
   /* eslint-enabled */
+
+  getListSql = (
+    limit: number,
+    offset?: number,
+    filters?: FilterInput,
+    sort?: SortInput[]
+  ): QuerySqlToken => {
+    const tableIdentifier = createTableIdentifier(this.table, this.schema);
+
+    return sql.unsafe`
+      SELECT
+        ${tableIdentifier}.*,
+        COALESCE(user_role.role, '{}') AS role
+      FROM ${tableIdentifier}
+      LEFT JOIN LATERAL (
+        SELECT json_agg(ur.role)
+        AS role FROM "public"."st__user_roles" as ur
+        WHERE ur.user_id = users.id
+      ) AS user_role
+      ON TRUE
+      ${createFilterFragment(filters, tableIdentifier)}
+      ${createSortFragment(tableIdentifier, this.getSortInput(sort))}
+      ${createLimitFragment(limit, offset)};
+    `;
+  };
 }
 
 export default UserSqlFactory;
