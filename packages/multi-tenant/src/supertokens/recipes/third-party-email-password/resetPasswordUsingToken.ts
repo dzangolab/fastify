68cfdaa23b5f99d051f1e099dfb4791a01fe522a
Dipendra Upreti <dipendra@dzangolab.com>
2023-06-06 18:15:14 +0545

    feat(multi-tenant): support reset password confirmation for multi-tenant

---
+++ packages/multi-tenant/src/supertokens/recipes/third-party-email-password/resetPasswordUsingToken.ts
@@ -0,0 +1,38 @@
+import { getUserById } from "supertokens-node/recipe/thirdpartyemailpassword";
+
+import mailer from "../../utils/sendEmail";
+
+import type { FastifyInstance } from "fastify";
+import type { RecipeInterface } from "supertokens-node/recipe/thirdpartyemailpassword/types";
+
+const resetPasswordUsingToken = (
+  originalImplementation: RecipeInterface,
+  fastify: FastifyInstance
+): RecipeInterface["resetPasswordUsingToken"] => {
+  return async (input) => {
+    const originalResponse =
+      await originalImplementation.resetPasswordUsingToken(input);
+
+    if (originalResponse.status === "OK" && originalResponse.userId) {
+      const user = await getUserById(originalResponse.userId, {
+        tenant: input.userContext._default.request.request.tenant,
+      });
+
+      if (user) {
+        mailer({
+          fastify,
+          subject: "Reset Password Confirmation",
+          templateName: "reset-password-confirmation",
+          to: user.email,
+          templateData: {
+            emailId: user.email,
+          },
+        });
+      }
+    }
+
+    return originalResponse;
+  };
+};
+
+export default resetPasswordUsingToken;
