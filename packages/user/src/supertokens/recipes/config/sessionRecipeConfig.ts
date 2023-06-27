c91034adca754baf746ba22132583851e123ce3e
Ramesh <ramesh@dzangolab.com>
2023-06-27 11:12:13 +0545

    feat(user): upgrade supertokens node to 13.6.0 (#419)
    * chore(deps): update supertokens-node package to 13.6.0
    
    * feat(user): set default token transfer method to cookie

--- packages/user/src/supertokens/recipes/config/sessionRecipeConfig.ts
+++ packages/user/src/supertokens/recipes/config/sessionRecipeConfig.ts
@@ -1,7 +1,13 @@
 import type { TypeInput as SessionRecipeConfig } from "supertokens-node/recipe/session/types";
 
 const getSessionRecipeConfig = (): SessionRecipeConfig => {
-  return {};
+  return {
+    getTokenTransferMethod: (input) => {
+      return input.req.getHeaderValue("st-auth-mode") === "header"
+        ? "header"
+        : "cookie";
+    },
+  };
 };
 
 export default getSessionRecipeConfig;
