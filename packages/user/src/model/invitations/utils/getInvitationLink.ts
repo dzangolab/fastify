cdac1316e63183619d10cbcebe8e989c87383639
Dipendra Upreti <dipendra@dzangolab.com>
2023-07-19 13:33:06 +0545

    chore(user): update variable name

--- packages/user/src/model/invitations/utils/getInvitationLink.ts
+++ packages/user/src/model/invitations/utils/getInvitationLink.ts
@@ -3,10 +3,10 @@ import type { ApiConfig } from "@dzangolab/fastify-config";
 const getInvitationLink = (
   config: ApiConfig,
   token: string,
-  appDomain: string
+  origin: string
 ): string => {
   // [DU 2023-JUL-19] Update the path from config
-  return `${appDomain}/register/token/${token}`;
+  return `${origin}/register/token/${token}`;
 };
 
 export default getInvitationLink;
