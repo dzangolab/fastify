b54eb3dcabea623307c94251fabae6e306d90bfc
Dipendra Upreti <dipendra@dzangolab.com>
2023-07-24 17:34:39 +0545

    refactor(user): rename variable

--- packages/user/src/model/invitations/handlers/resendInvitation.ts
+++ packages/user/src/model/invitations/handlers/resendInvitation.ts
@@ -61,11 +61,11 @@ const resendInvitation = async (
         log.error(error);
       }
 
-      const response: Partial<Invitation> = invitation;
+      const data: Partial<Invitation> = invitation;
 
-      delete response.token;
+      delete data.token;
 
-      reply.send(response);
+      reply.send(data);
     }
   } catch (error) {
     log.error(error);
