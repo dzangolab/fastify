43e768e00f8a8459fd1e40d95c91fba8ca5de1a5
Dipendra Upreti <dipendra@dzangolab.com>
2023-08-10 14:53:29 +0545

    refactor(user): update constants.ts

--- packages/user/src/constants.ts
+++ packages/user/src/constants.ts
@@ -1,39 +1,36 @@
+// Invitations
 const INVITATION_ACCEPT_PATH = "/register/token";
 const INVITATION_EXPIRE_AFTER_IN_DAYS = 30;
-
-const RESET_PASSWORD_PATH = "/auth/reset-password";
-
-// Invitations
-const TABLE_INVITATIONS = "invitations";
 const ROUTE_INVITATIONS = "/invitations";
 const ROUTE_INVITATIONS_ACCEPT = "/invitations/token/:token";
 const ROUTE_INVITATIONS_CREATE = "/invitations";
 const ROUTE_INVITATIONS_GET_BY_TOKEN = "/invitations/token/:token";
 const ROUTE_INVITATIONS_RESEND = "/invitations/resend/:id(^\\d+)";
 const ROUTE_INVITATIONS_REVOKE = "/invitations/revoke/:id(^\\d+)";
+const TABLE_INVITATIONS = "invitations";
 
 // Users
-const TABLE_USERS = "users";
+const DEFAULT_ROLE = "USER";
+const RESET_PASSWORD_PATH = "/auth/reset-password";
 const ROUTE_CHANGE_PASSWORD = "/change_password";
 const ROUTE_ME = "/me";
 const ROUTE_USERS = "/users";
-
-const DEFAULT_ROLE = "USER";
+const TABLE_USERS = "users";
 
 export {
   DEFAULT_ROLE,
-  TABLE_INVITATIONS,
   INVITATION_ACCEPT_PATH,
   INVITATION_EXPIRE_AFTER_IN_DAYS,
   RESET_PASSWORD_PATH,
-  TABLE_USERS,
+  ROUTE_CHANGE_PASSWORD,
   ROUTE_INVITATIONS,
   ROUTE_INVITATIONS_ACCEPT,
   ROUTE_INVITATIONS_CREATE,
   ROUTE_INVITATIONS_GET_BY_TOKEN,
   ROUTE_INVITATIONS_RESEND,
   ROUTE_INVITATIONS_REVOKE,
-  ROUTE_CHANGE_PASSWORD,
   ROUTE_ME,
   ROUTE_USERS,
+  TABLE_INVITATIONS,
+  TABLE_USERS,
 };
