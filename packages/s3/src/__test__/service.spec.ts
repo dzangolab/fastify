f1f1e8c2cc49b86ff79ee69c1bbc36e8299913ae
Suraj Rai <67699019+surajrai-dzango@users.noreply.github.com>
2023-08-16 18:14:57 +0545

    feat: added dzangolab/fastify-s3 package (#464)
    * feat: add fastify file manager package
    
    * feat: add fastify file manager package
    
    * feat: update file manager to s3
    
    * fix: update file scheme

---
+++ packages/s3/src/__test__/service.spec.ts
@@ -0,0 +1,8 @@
+/* istanbul ignore file */
+import { describe, expect, it } from "vitest";
+
+describe("File Service", () => {
+  it("should create a file instance with augmented fields", () => {
+    expect(false).toBe(false);
+  });
+});
---
+++ packages/s3/src/index.ts
@@ -0,0 +1,17 @@
+import type { ServerResponse } from "node:http";
+
+declare module "fastify" {
+  interface FastifyReply {
+    setHeader: ServerResponse["setHeader"];
+  }
+}
+
+declare module "@dzangolab/fastify-config" {
+  interface ApiConfig {
+    file: null;
+  }
+}
+
+export { default as FileService } from "./model/files/service";
+
+export type { File, FileCreateInput, FileUpdateInput } from "./types";
