964e2b4095a8b760cb06ef87f170b23d06ed6b7e
Dipendra Upreti <91712746+dipendraupreti@users.noreply.github.com>
2023-09-13 16:12:26 +0545

    fix(user): fix graphql issue when email not verified for public endpoint (#493)

--- packages/user/src/mercurius-auth/plugin.ts
+++ packages/user/src/mercurius-auth/plugin.ts
@@ -1,6 +1,7 @@
 import FastifyPlugin from "fastify-plugin";
 import mercurius from "mercurius";
 import mercuriusAuth from "mercurius-auth";
+import emailVerificaiton from "supertokens-node/recipe/emailverification";
 
 import type { FastifyInstance } from "fastify";
 
@@ -11,7 +12,31 @@ const plugin = FastifyPlugin(async (fastify: FastifyInstance) => {
     await fastify.register(mercuriusAuth, {
       async applyPolicy(authDirectiveAST, parent, arguments_, context) {
         if (!context.user) {
-          return new mercurius.ErrorWithProps("unauthorized", {}, 200);
+          return new mercurius.ErrorWithProps("unauthorized", {}, 401);
+        }
+
+        if (
+          fastify.config.user.features?.signUp?.emailVerification &&
+          !(await emailVerificaiton.isEmailVerified(context.user.id))
+        ) {
+          // Added the claim validation errors to match with rest endpoint
+          // response for email verification
+          return new mercurius.ErrorWithProps(
+            "invalid claim",
+            {
+              claimValidationErrors: [
+                {
+                  id: "st-ev",
+                  reason: {
+                    message: "wrong value",
+                    expectedValue: true,
+                    actualValue: false,
+                  },
+                },
+              ],
+            },
+            403
+          );
         }
 
         return true;
