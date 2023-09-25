fd7e833521d73f1a4a96ef387317f3bdf11d0245
Suraj Rai <67699019+surajrai-dzango@users.noreply.github.com>
2023-09-25 18:26:47 +0545

    fix: export multipart parser plugin (#520)

--- packages/s3/src/index.ts
+++ packages/s3/src/index.ts
@@ -23,5 +23,7 @@ export { default as FileService } from "./model/files/service";
 export { default as S3Client } from "./utils/s3Client";
 export type { FilePayload, Multipart } from "./types";
 export type { File, FileCreateInput, FileUpdateInput } from "./types/file";
+export type { FileUpload as GraphQLFileUpload } from "graphql-upload-minimal";
 
 export { default } from "./plugin";
+export { default as multipartParserPlugin } from "./plugins/multipartParser";
--- packages/s3/src/plugins/multipartParser.ts
+++ packages/s3/src/plugins/multipartParser.ts
@@ -1,10 +1,6 @@
-import { IncomingMessage } from "node:http";
-import { Readable } from "node:stream";
-
-import Busboy, { FileInfo } from "busboy";
 import fastifyPlugin from "fastify-plugin";
 
-import { Multipart } from "../types";
+import { processMultipartFormData } from "../utils";
 
 import type { FastifyInstance, FastifyRequest } from "fastify";
 
@@ -21,7 +17,10 @@ const plugin = (
 ) => {
   if (!fastify.hasContentTypeParser("multipart")) {
     fastify.addContentTypeParser("multipart", (req, _payload, done) => {
-      if (req.routerPath.startsWith(fastify.config.mercurius.path as string)) {
+      if (
+        req.config.mercurius.enabled &&
+        req.routerPath.startsWith(req.config.mercurius.path as string)
+      ) {
         req.mercuriusUploadMultipart = true;
 
         // eslint-disable-next-line unicorn/no-null
@@ -33,64 +32,6 @@ const plugin = (
   }
 
   done();
-};
-
-const processMultipartFormData = (
-  req: FastifyRequest,
-  _payload: IncomingMessage,
-  done: (err: Error | null, body?: unknown) => void
-) => {
-  const busboyParser = Busboy({
-    headers: req.headers,
-  });
-
-  const fields: Record<string, string> = {};
-  const files: Record<string, Multipart[]> = {};
-
-  busboyParser.on("field", (fieldName, value) => {
-    fields[fieldName] = value;
-  });
-
-  busboyParser.on(
-    "file",
-    (fieldName: string, file: Readable, fileInfo: FileInfo) => {
-      const chunks: Buffer[] = [];
-
-      file.on("data", (chunk: Buffer) => {
-        chunks.push(chunk);
-      });
-
-      file.on("end", () => {
-        const fileBuffer = Buffer.concat(chunks);
-
-        if (!files[fieldName]) {
-          files[fieldName] = [];
-        }
-
-        files[fieldName].push({
-          ...fileInfo,
-          mimetype: fileInfo.mimeType,
-          data: fileBuffer,
-        });
-      });
-    }
-  );
-
-  busboyParser.on("finish", () => {
-    req.body = {
-      ...fields,
-      ...files,
-    };
-
-    // eslint-disable-next-line unicorn/no-null
-    done(null, req.body);
-  });
-
-  busboyParser.on("error", (err) => {
-    console.log(err);
-  });
-
-  _payload.pipe(busboyParser);
 };
 
 export default fastifyPlugin(plugin);
