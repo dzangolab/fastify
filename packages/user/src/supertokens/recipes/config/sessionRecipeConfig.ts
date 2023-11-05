3da0eba4b27615ac0643f106bef44386513384fd
Dipendra Upreti <91712746+dipendraupreti@users.noreply.github.com>
2023-11-05 13:25:29 +0545

    block disabled user using verifySession  (#537)

--- packages/user/src/supertokens/recipes/config/sessionRecipeConfig.ts
+++ packages/user/src/supertokens/recipes/config/sessionRecipeConfig.ts
@@ -1,11 +1,31 @@
+import createNewSession from "./session/createNewSession";
+import verifySession from "./session/verifySession";
+
+import type { FastifyInstance } from "fastify";
 import type { TypeInput as SessionRecipeConfig } from "supertokens-node/recipe/session/types";
 
-const getSessionRecipeConfig = (): SessionRecipeConfig => {
+const getSessionRecipeConfig = (
+  fastify: FastifyInstance
+): SessionRecipeConfig => {
   return {
     getTokenTransferMethod: (input) => {
       return input.req.getHeaderValue("st-auth-mode") === "header"
         ? "header"
         : "cookie";
+    },
+    override: {
+      apis: (originalImplementation) => {
+        return {
+          ...originalImplementation,
+          verifySession: verifySession(originalImplementation, fastify),
+        };
+      },
+      functions: (originalImplementation) => {
+        return {
+          ...originalImplementation,
+          createNewSession: createNewSession(originalImplementation, fastify),
+        };
+      },
     },
   };
 };
