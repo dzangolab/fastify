df021110c6d93b1f819b661487da4245481d7f52
Dipendra Upreti <91712746+dipendraupreti@users.noreply.github.com>
2023-11-08 14:07:18 +0545

    feat(user): make supertokens session recipe configurable (#546)

--- packages/user/src/supertokens/recipes/config/sessionRecipeConfig.ts
+++ packages/user/src/supertokens/recipes/config/sessionRecipeConfig.ts
@@ -1,13 +1,27 @@
 import createNewSession from "./session/createNewSession";
 import verifySession from "./session/verifySession";
 
+import type { SessionRecipe } from "../../types/sessionRecipe";
 import type { FastifyInstance } from "fastify";
-import type { TypeInput as SessionRecipeConfig } from "supertokens-node/recipe/session/types";
+import type {
+  APIInterface,
+  RecipeInterface,
+  TypeInput as SessionRecipeConfig,
+} from "supertokens-node/recipe/session/types";
 
 const getSessionRecipeConfig = (
   fastify: FastifyInstance
 ): SessionRecipeConfig => {
+  const { config } = fastify;
+
+  let session: SessionRecipe = {};
+
+  if (typeof config.user.supertokens.recipes?.session === "object") {
+    session = config.user.supertokens.recipes.session;
+  }
+
   return {
+    ...session,
     getTokenTransferMethod: (input) => {
       return input.req.getHeaderValue("st-auth-mode") === "header"
         ? "header"
@@ -15,17 +29,59 @@ const getSessionRecipeConfig = (
     },
     override: {
       apis: (originalImplementation) => {
+        const apiInterface: Partial<APIInterface> = {};
+
+        if (session.override?.apis) {
+          const apis = session.override.apis;
+
+          let api: keyof APIInterface;
+
+          for (api in apis) {
+            const apiWrapper = apis[api];
+
+            if (apiWrapper) {
+              apiInterface[api] = apiWrapper(
+                originalImplementation,
+                fastify
+                // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+              ) as any;
+            }
+          }
+        }
         return {
           ...originalImplementation,
           verifySession: verifySession(originalImplementation, fastify),
+          ...apiInterface,
         };
       },
       functions: (originalImplementation) => {
+        const recipeInterface: Partial<RecipeInterface> = {};
+
+        if (session.override?.functions) {
+          const recipes = session.override.functions;
+
+          let recipe: keyof RecipeInterface;
+
+          for (recipe in recipes) {
+            const recipeWrapper = recipes[recipe];
+
+            if (recipeWrapper) {
+              recipeInterface[recipe] = recipeWrapper(
+                originalImplementation,
+                fastify
+                // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+              ) as any;
+            }
+          }
+        }
+
         return {
           ...originalImplementation,
           createNewSession: createNewSession(originalImplementation, fastify),
+          ...recipeInterface,
         };
       },
+      openIdFeature: session.override?.openIdFeature,
     },
   };
 };
