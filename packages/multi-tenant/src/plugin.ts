fe17c11bbaf56fa03912b5debff3259201c7be3d
Dipendra Upreti <dipendra@dzangolab.com>
2023-11-20 14:19:35 +0545

    refactor(multi-tenant): export tenantMigrationPlugin

--- packages/multi-tenant/src/plugin.ts
+++ packages/multi-tenant/src/plugin.ts
@@ -2,7 +2,6 @@ import FastifyPlugin from "fastify-plugin";
 import merge from "lodash.merge";
 
 import updateContext from "./lib/updateContext";
-import migratePlugin from "./migratePlugin";
 import thirdPartyEmailPasswordConfig from "./supertokens/recipes";
 import tenantDiscoveryPlugin from "./tenantDiscoveryPlugin";
 
@@ -15,9 +14,6 @@ const plugin = async (
   done: () => void
 ) => {
   fastify.log.info("Registering fastify-multi-tenant plugin");
-
-  // Register migrate plugin
-  await fastify.register(migratePlugin);
 
   // Register domain discovery plugin
   await fastify.register(tenantDiscoveryPlugin);
