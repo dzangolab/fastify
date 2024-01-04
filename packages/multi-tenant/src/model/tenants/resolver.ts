95bb1f96ac1b4a3218047a6aa219eb00dfe67d89
Dipendra Upreti <91712746+dipendraupreti@users.noreply.github.com>
2024-01-04 16:52:07 +0545

    feat(multi-tenant): add tenant controller and resolver (#574)

---
+++ packages/multi-tenant/src/model/tenants/resolver.ts
@@ -0,0 +1,74 @@
+import Service from "./service";
+import { validateTenantInput } from "../../lib/validateTenantSchema";
+
+import type { TenantCreateInput } from "./../../types";
+import type { FilterInput, SortInput } from "@dzangolab/fastify-slonik";
+import type { MercuriusContext } from "mercurius";
+
+const Mutation = {
+  createTenant: async (
+    parent: unknown,
+    arguments_: {
+      data: {
+        id: string;
+        password: string;
+      };
+    },
+    context: MercuriusContext
+  ) => {
+    const input = arguments_.data as TenantCreateInput;
+
+    validateTenantInput(context.config, input);
+
+    const service = new Service(
+      context.config,
+      context.database,
+      context.dbSchema
+    );
+
+    return await service.create(input);
+  },
+};
+
+const Query = {
+  tenant: async (
+    parent: unknown,
+    arguments_: { id: number },
+    context: MercuriusContext
+  ) => {
+    const service = new Service(
+      context.config,
+      context.database,
+      context.dbSchema
+    );
+
+    return await service.findById(arguments_.id);
+  },
+  tenants: async (
+    parent: unknown,
+    arguments_: {
+      limit: number;
+      offset: number;
+      filters?: FilterInput;
+      sort?: SortInput[];
+    },
+    context: MercuriusContext
+  ) => {
+    const service = new Service(
+      context.config,
+      context.database,
+      context.dbSchema
+    );
+
+    return await service.list(
+      arguments_.limit,
+      arguments_.offset,
+      arguments_.filters
+        ? JSON.parse(JSON.stringify(arguments_.filters))
+        : undefined,
+      arguments_.sort ? JSON.parse(JSON.stringify(arguments_.sort)) : undefined
+    );
+  },
+};
+
+export default { Mutation, Query };
