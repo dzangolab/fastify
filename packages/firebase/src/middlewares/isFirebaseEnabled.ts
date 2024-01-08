cba9fbaa9f2c35cb72e3d6abe463516c29626275
Sudeep Bashistha <sudeep@dzangolab.com>
2024-01-08 16:28:56 +0545

    refactor: remove variable from isFirebaseEnabled middleware:

--- packages/firebase/src/middlewares/isFirebaseEnabled.ts
+++ packages/firebase/src/middlewares/isFirebaseEnabled.ts
@@ -2,9 +2,7 @@ import { FastifyInstance } from "fastify";
 
 const isFirebaseEnabled =
   (fastify: FastifyInstance) => async (): Promise<void> => {
-    const isEnabled = fastify.config.firebase.enabled;
-
-    if (isEnabled === false) {
+    if (fastify.config.firebase.enabled === false) {
       throw new Error("Firebase is not enabled");
     }
   };
