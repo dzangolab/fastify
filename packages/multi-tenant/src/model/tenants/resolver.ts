89adb4fae53de6073d2fa6d90d6c57ed9f80223d
Dipendra Upreti <dipendra@dzangolab.com>
2024-01-12 21:49:42 +0545

    feat(multi-tenant): update tenant and tenants resolvers

--- packages/multi-tenant/src/model/tenants/resolver.ts
+++ packages/multi-tenant/src/model/tenants/resolver.ts
@@ -1,6 +1,8 @@
 import mercurius from "mercurius";
+import UserRoles from "supertokens-node/recipe/userroles";
 
 import Service from "./service";
+import { ROLE_TENANT_OWNER } from "../../constants";
 import getMultiTenantConfig from "../../lib/getMultiTenantConfig";
 import { validateTenantInput } from "../../lib/validateTenantSchema";
 
@@ -75,13 +77,33 @@ const Query = {
       );
     }
 
-    const service = new Service(
-      context.config,
-      context.database,
-      context.dbSchema
-    );
+    const userId = context.user?.id;
 
-    return await service.findById(arguments_.id);
+    if (userId) {
+      const service = new Service(
+        context.config,
+        context.database,
+        context.dbSchema
+      );
+
+      const { roles } = await UserRoles.getRolesForUser(userId);
+
+      if (roles.includes(ROLE_TENANT_OWNER)) {
+        service.ownerId = userId;
+      }
+
+      return await service.findById(arguments_.id);
+    } else {
+      context.app.log.error(
+        "Could not able to get user id from mercurius context"
+      );
+
+      return new mercurius.ErrorWithProps(
+        "Oops, Something went wrong",
+        undefined,
+        500
+      );
+    }
   },
   tenants: async (
     parent: unknown,
@@ -94,6 +116,10 @@ const Query = {
     context: MercuriusContext
   ) => {
     if (context.tenant) {
+      context.app.log.error(
+        "Could not able to get user id from mercurius context"
+      );
+
       return new mercurius.ErrorWithProps(
         "Tenant app cannot display a list of tenants",
         undefined,
@@ -101,20 +127,38 @@ const Query = {
       );
     }
 
-    const service = new Service(
-      context.config,
-      context.database,
-      context.dbSchema
-    );
+    const userId = context.user?.id;
 
-    return await service.list(
-      arguments_.limit,
-      arguments_.offset,
-      arguments_.filters
-        ? JSON.parse(JSON.stringify(arguments_.filters))
-        : undefined,
-      arguments_.sort ? JSON.parse(JSON.stringify(arguments_.sort)) : undefined
-    );
+    if (userId) {
+      const service = new Service(
+        context.config,
+        context.database,
+        context.dbSchema
+      );
+
+      const { roles } = await UserRoles.getRolesForUser(userId);
+
+      if (roles.includes(ROLE_TENANT_OWNER)) {
+        service.ownerId = userId;
+      }
+
+      return await service.list(
+        arguments_.limit,
+        arguments_.offset,
+        arguments_.filters
+          ? JSON.parse(JSON.stringify(arguments_.filters))
+          : undefined,
+        arguments_.sort
+          ? JSON.parse(JSON.stringify(arguments_.sort))
+          : undefined
+      );
+    } else {
+      return new mercurius.ErrorWithProps(
+        "Oops, Something went wrong",
+        undefined,
+        500
+      );
+    }
   },
 };
 
