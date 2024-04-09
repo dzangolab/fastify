7e5c40918686a3ba74b9a117f4dedda9c0732e9f
Dipendra Upreti <dipendra@dzangolab.com>
2024-04-09 17:47:25 +0545

    refactor(user): change role to supertokens role

--- packages/user/src/index.ts
+++ packages/user/src/index.ts
@@ -102,9 +102,9 @@ export { default as getInvitationService } from "./lib/getInvitationService";
 export { default as invitationRoutes } from "./model/invitations/controller";
 export { default as permissionResolver } from "./model/permissions/resolver";
 export { default as permissionRoutes } from "./model/permissions/controller";
-export { default as RoleService } from "./model/roles/service";
-export { default as roleResolver } from "./model/roles/resolver";
-export { default as roleRoutes } from "./model/roles/controller";
+export { default as RoleService } from "./model/supertokens-roles/service";
+export { default as roleResolver } from "./model/supertokens-roles/resolver";
+export { default as roleRoutes } from "./model/supertokens-roles/controller";
 // [DU 2023-AUG-07] use formatDate from "@dzangolab/fastify-slonik" package
 export { formatDate } from "@dzangolab/fastify-slonik";
 export { default as computeInvitationExpiresAt } from "./lib/computeInvitationExpiresAt";
