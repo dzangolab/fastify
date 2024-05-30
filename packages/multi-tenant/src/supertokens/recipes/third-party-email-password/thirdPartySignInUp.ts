422ea437de8cd7eef6b6b0c7d1a35e553ca17408
Dipendra Upreti <91712746+dipendraupreti@users.noreply.github.com>
2024-05-30 17:38:42 +0545

    fix(user): update profile validation claim in sesson for me (#670)

--- packages/multi-tenant/src/supertokens/recipes/third-party-email-password/thirdPartySignInUp.ts
+++ packages/multi-tenant/src/supertokens/recipes/third-party-email-password/thirdPartySignInUp.ts
@@ -1,11 +1,13 @@
-import { areRolesExist } from "@dzangolab/fastify-user";
+import { areRolesExist, formatDate } from "@dzangolab/fastify-user";
 import { deleteUser } from "supertokens-node";
 import { getUserByThirdPartyInfo } from "supertokens-node/recipe/thirdpartyemailpassword";
 import UserRoles from "supertokens-node/recipe/userroles";
 
 import getMultiTenantConfig from "../../../lib/getMultiTenantConfig";
+import getUserService from "../../../lib/getUserService";
 
 import type { Tenant } from "../../../types";
+import type { User } from "@dzangolab/fastify-user";
 import type { FastifyInstance, FastifyError } from "fastify";
 import type { RecipeInterface } from "supertokens-node/recipe/thirdpartyemailpassword";
 
@@ -13,7 +15,7 @@ const thirdPartySignInUp = (
   originalImplementation: RecipeInterface,
   fastify: FastifyInstance
 ): RecipeInterface["thirdPartySignInUp"] => {
-  const { config, log } = fastify;
+  const { config, log, slonik } = fastify;
 
   return async (input) => {
     const roles = (input.userContext.roles || []) as string[];
@@ -43,7 +45,9 @@ const thirdPartySignInUp = (
       input
     );
 
-    if (originalResponse.status === "OK" && originalResponse.createdNewUser) {
+    const userService = getUserService(config, slonik, tenant);
+
+    if (originalResponse.createdNewUser) {
       if (!(await areRolesExist(roles))) {
         await deleteUser(originalResponse.user.id);
 
@@ -66,6 +70,43 @@ const thirdPartySignInUp = (
           log.error(rolesResponse.status);
         }
       }
+
+      let user: User | null | undefined;
+
+      try {
+        user = await userService.create({
+          id: originalResponse.user.id,
+          email: originalResponse.user.email,
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+        /*eslint-disable-next-line @typescript-eslint/no-explicit-any */
+      } catch (error: any) {
+        log.error("Error while creating user");
+        log.error(error);
+
+        await deleteUser(originalResponse.user.id);
+
+        throw {
+          name: "SIGN_UP_FAILED",
+          message: "Something went wrong",
+          statusCode: 500,
+        };
+      }
+    } else {
+      await userService
+        .update(originalResponse.user.id, {
+          lastLoginAt: formatDate(new Date(Date.now())),
+        })
+        /*eslint-disable-next-line @typescript-eslint/no-explicit-any */
+        .catch((error: any) => {
+          log.error(
+            `Unable to update lastLoginAt for userId ${originalResponse.user.id}`
+          );
+          log.error(error);
+        });
     }
 
     return originalResponse;
