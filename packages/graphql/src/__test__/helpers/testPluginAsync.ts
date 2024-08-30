1b6e269ecfaa914adc484d59d86d8a71492dc8cc
Dipendra Upreti <dipendra@dzangolab.com>
2024-08-30 19:23:43 +0545

    chore: reomove unknown keyword

--- packages/graphql/src/__test__/helpers/testPluginAsync.ts
+++ packages/graphql/src/__test__/helpers/testPluginAsync.ts
@@ -18,7 +18,7 @@ declare module "fastify" {
 
 const plugin = FastifyPlugin(async (fastify: FastifyInstance) => {
   fastify.decorate("propertyOne", "Property One");
-}) as unknown as GraphqlEnabledPlugin;
+}) as GraphqlEnabledPlugin;
 
 plugin.updateContext = async (context: MercuriusContext) => {
   context.propertyOne = "Property One";
