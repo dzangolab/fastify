69640c87390455c83bfa3bcdf71ca6b3e858880f
Dipendra <91712746+dipendraupreti@users.noreply.github.com>
2024-09-11 10:47:50 +0545

    Feat: add organization_users table (#709)

--- packages/user/src/migrations/runMigrations.ts
+++ packages/user/src/migrations/runMigrations.ts
@@ -3,6 +3,7 @@ import { Database } from "@dzangolab/fastify-slonik";
 import createAccountsQuery from "./createAccountsQuery";
 import createOrganizationOwnersQuery from "./createOrganizationOwnersQuery";
 import createOrganizationsQuery from "./createOrganizationsQuery";
+import createOrganizationUsersQuery from "./createOrganizationUsersQuery";
 
 const runMigrations = async (database: Database) => {
   await database.connect(async (connection) => {
@@ -15,6 +16,10 @@ const runMigrations = async (database: Database) => {
 
   await database.connect(async (connection) => {
     await connection.query(createAccountsQuery());
+  });
+
+  await database.connect(async (connection) => {
+    await connection.query(createOrganizationUsersQuery());
   });
 };
 
