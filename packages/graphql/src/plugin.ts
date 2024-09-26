b6faf81c0f63b684f251b4e0dd25c43d707fb19b
Dipendra <91712746+dipendraupreti@users.noreply.github.com>
2024-09-26 21:10:08 +0545

    feat(graphql): support options as argument by graphql plugin (#779)

--- packages/graphql/src/plugin.ts
+++ packages/graphql/src/plugin.ts
@@ -3,16 +3,31 @@ import mercurius from "mercurius";
 
 import buildContext from "./buildContext";
 
+import type { GraphqlOptions } from "./types";
 import type { FastifyInstance } from "fastify";
 
-const plugin = async (fastify: FastifyInstance) => {
-  const config = fastify.config.graphql;
+const plugin = async (fastify: FastifyInstance, options: GraphqlOptions) => {
+  fastify.log.info("Registering fastify-graphql plugin");
 
-  if (config?.enabled) {
+  if (Object.keys(options).length === 0) {
+    fastify.log.warn(
+      "The graphql plugin now recommends passing graphql options directly to the plugin.",
+    );
+
+    if (!fastify.config?.graphql) {
+      throw new Error(
+        "Missing graphql configuration. Did you forget to pass it to the graphql plugin?",
+      );
+    }
+
+    options = fastify.config.graphql;
+  }
+
+  if (options?.enabled) {
     // Register graphql
     await fastify.register(mercurius, {
-      context: buildContext,
-      ...config,
+      context: buildContext(options.plugins),
+      ...options,
     });
   } else {
     fastify.log.info("GraphQL API not enabled");
