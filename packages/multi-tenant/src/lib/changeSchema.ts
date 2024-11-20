fb93d02e815f9fc1054979622939e6a680e9aee7
Ramesh <ramesh@dzangolab.com>
2024-11-20 21:35:09 +0545

    fix: fix change schema query

--- packages/multi-tenant/src/lib/changeSchema.ts
+++ packages/multi-tenant/src/lib/changeSchema.ts
@@ -4,8 +4,8 @@ const changeSchema = async (client: Client, name: string) => {
   // Create schema if not exists and switch to the schema
   await client.query(
     `
-      CREATE SCHEMA IF NOT EXISTS ${name};
-      SET search_path TO ${name};
+      CREATE SCHEMA IF NOT EXISTS "${name}";
+      SET search_path TO "${name}";
     `,
   );
 };
