717ab365421b204b6818b93ef478c1bae025c66c
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2025-05-08 12:59:02 +0545

    refactor: create separate component for filters

--- packages/user/src/model/users/sql.ts
+++ packages/user/src/model/users/sql.ts
@@ -1,12 +1,9 @@
-import { applyFilter } from "@dzangolab/fastify-slonik";
 import humps from "humps";
 import { sql } from "slonik";
 
-import type {
-  BaseFilterInput,
-  FilterInput,
-  SortInput,
-} from "@dzangolab/fastify-slonik";
+import { applyFiltersToQuery } from "./dbFilters";
+
+import type { FilterInput, SortInput } from "@dzangolab/fastify-slonik";
 import type { FragmentSqlToken, IdentifierSqlToken } from "slonik";
 
 const createSortFragment = (
@@ -63,80 +60,6 @@ const createSortRoleFragment = (
   });
 
   return sql.fragment`ORDER BY ${identifier} ${direction}`;
-};
-
-const applyFiltersToQuery = (
-  filters: FilterInput,
-  tableIdentifier: IdentifierSqlToken,
-  not = false,
-): FragmentSqlToken => {
-  const andFilter: FragmentSqlToken[] = [];
-  const orFilter: FragmentSqlToken[] = [];
-  let queryFilter;
-
-  const applyFilters = (
-    filters: FilterInput,
-    tableIdentifier: IdentifierSqlToken,
-    not = false,
-  ) => {
-    if ("AND" in filters) {
-      for (const filterData of filters.AND) {
-        applyFilters(filterData, tableIdentifier);
-      }
-    } else if ("OR" in filters) {
-      for (const filterData of filters.OR) {
-        applyFilters(filterData, tableIdentifier, true);
-      }
-    } else {
-      const query =
-        humps.decamelize(filters.key) === "roles"
-          ? applyRolesFilter(filters)
-          : applyFilter(tableIdentifier, filters);
-
-      if (not) {
-        orFilter.push(query);
-      } else {
-        andFilter.push(query);
-      }
-    }
-  };
-
-  applyFilters(filters, tableIdentifier, not);
-
-  if (andFilter.length > 0 && orFilter.length > 0) {
-    queryFilter = sql.join(
-      [
-        sql.fragment`(${sql.join(andFilter, sql.fragment` AND `)})`,
-        sql.fragment`(${sql.join(orFilter, sql.fragment` OR `)})`,
-      ],
-      sql.fragment`${
-        "AND" in filters ? sql.fragment` AND ` : sql.fragment` OR `
-      }`,
-    );
-  } else if (andFilter.length > 0) {
-    queryFilter = sql.join(andFilter, sql.fragment` AND `);
-  } else if (orFilter.length > 0) {
-    queryFilter = sql.join(orFilter, sql.fragment` OR `);
-  }
-
-  return queryFilter ? sql.fragment`WHERE ${queryFilter}` : sql.fragment``;
-};
-
-const applyRolesFilter = (filter: BaseFilterInput) => {
-  const { value, not = false } = filter;
-
-  const notFragment = not ? sql.fragment`NOT` : sql.fragment``;
-
-  const valueFragment = sql.fragment`(${sql.join(
-    value.split(","),
-    sql.fragment`, `,
-  )})`;
-
-  return sql.fragment`${notFragment} EXISTS (
-    SELECT roles
-    FROM jsonb_array_elements_text(user_role.role) as roles
-    WHERE roles IN ${valueFragment}
-  )`;
 };
 
 const filterFragment = (
